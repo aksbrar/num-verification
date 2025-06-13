6a262e15c9c126fdb65013e63d50950f5bc291d6
aksbrar <aksbrar34@gmail.com>
2025-06-13 16:53:31 -0400

    added country names and codes into options to select for user

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
+// Imports
 import { countries } from "./country.js";
+
+// constant Variables
+const counOption = document.querySelector("#options");
+
+// add country names and codes into options
+counOption.innerHTML += countries.map(c => {
+  if (c.name.length>15){
+    let slicedName = c.name.slice(0, 15)
+    return `<option value="${c.code}">${slicedName}(${c.code})</option>`
+  }
+  else {
+    return `<option value="${c.code}">${c.name}(${c.code})</option>`
+  }
+}).join("")
+
+
+
